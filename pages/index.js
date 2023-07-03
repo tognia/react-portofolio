807baa1e0e102c8dd8844c97b5401a9122f1dc33
togniahenri <henribpeace@gmail.com>
2023-07-03 10:54:11 +0100

    03072023 commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 import Head from 'next/head';
 import {BsFillMoonStarsFill} from 'react-icons/bs';
+import {
+  AiFillTwitterCircle, 
+  AiFillLinkedin, 
+  AiFillYoutube} 
+from 'react-icons/ai';
 
 
 export default function Home() {
@@ -11,17 +16,34 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className="bg-white px-10">
         <section className="min-h-screen">
-          <nav>
-          <h1>Developed By TNH</h1>
-          <ul>
+          <nav className="py-10 mb-12 flex justify-between">
+          <h1 className='text-xl font-burtons'>Developed By TNH</h1>
+          <ul className='flex items-center'>
             <li>
-              <BsFillMoonStarsFill/>
+              <BsFillMoonStarsFill className='cursor-pointer text-2xl'/>
             </li>
-            <li><a href='#'>Resume</a></li>
+            <li><a className= 'bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href='#'>Resume</a></li>
           </ul>
-          </nav>          
+          </nav>   
+          <div className='text-center p-10'>
+            <h2 className='text-5xl'>TOGNIA Henri</h2>
+            <h3>Developer and designer</h3>
+            <p>
+            With these steps, you have manually created an IAM role in AWS with the necessary permissions and configured 
+            GitHub Actions to use that role for deployment. The secret credentials (AWS access key ID and secret access key) 
+            are not exposed in the workflow, and the workflow will use the specified IAM role and region for deploying the API Gateway API [^web page context^].
+            </p>
+          </div> 
+          <div>
+              <AiFillTwitterCircle />
+              <AiFillLinkedin />
+              <AiFillYoutube />
+          </div>  
+          <div>
+              
+          </div>      
 
         </section>
       </main>
